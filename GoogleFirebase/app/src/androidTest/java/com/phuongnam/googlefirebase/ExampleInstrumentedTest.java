27ea8bf79fe9e5265a3666d1aefc6efaae66c600
pnam272005-cell <pnam272005@gmail.com>
2026-05-07 12:59:21 +0700

    BÀI TẬP TUẦN HỌC SỐ

--- GoogleFirebase/app/src/androidTest/java/com/phuongnam/googlefirebase/ExampleInstrumentedTest.java
+++ GoogleFirebase/app/src/androidTest/java/com/phuongnam/googlefirebase/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.phuongnam.baith12_fragmentdong;
+package com.phuongnam.googlefirebase;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.phuongnam.baith12_fragmentdong", appContext.getPackageName());
+        assertEquals("com.phuongnam.googlefirebase", appContext.getPackageName());
     }
 }
